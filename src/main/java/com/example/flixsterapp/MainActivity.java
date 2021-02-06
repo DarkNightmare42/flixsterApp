455cbc994dea6ccc1b04c9b2ef8bb33675f5d174
Errol Roberts <errol.me@gmail.com>
2021-02-06 13:23:16 -0500

    initial commit

---
+++ src/main/java/com/example/flixsterapp/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.flixsterapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import okhttp3.Headers;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String sourceUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed";
+    public static final String tag = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get(sourceUrl, new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int i, Headers headers, JSON json) {
+                Log.d(tag, "onSuccess");
+                JSONObject jsonObject = json.jsonObject;
+                try {
+                    JSONArray results = jsonObject.getJSONArray("results");
+                    Log.i(tag, "Results: " + results.toString());
+                } catch (JSONException e) {
+                    Log.e(tag, "Hit json exception", e);
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int i, Headers headers, String s, Throwable throwable) {
+                Log.d(tag, "onFailure");
+            }
+        });
+    }
+}
---
+++ src/main/java/com/example/flixsterapp/models/movies.java
@@ -0,0 +1,4 @@
+package com.example.flixsterapp.models;
+
+public class movies {
+}
