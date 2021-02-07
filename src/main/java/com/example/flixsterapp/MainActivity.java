773ec3cc3b9c0dad2cacefe39af08000f45a0304
Errol Roberts <errol.me@gmail.com>
2021-02-07 12:36:47 -0500

    functional base app

--- src/main/java/com/example/flixsterapp/MainActivity.java
+++ src/main/java/com/example/flixsterapp/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.flixsterapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.util.Log;
 
 import com.codepath.asynchttpclient.AsyncHttpClient;
 import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+import com.example.flixsterapp.adapters.movieAdapter;
 import com.example.flixsterapp.models.movies;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import okhttp3.Headers;
@@ -24,10 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
     List<movies> movie;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        movie = new ArrayList<>();
+        RecyclerView rView = findViewById(R.id.rView);
+
+        movieAdapter MovieAdapter = new movieAdapter(this, movie);
+
+        rView.setAdapter(MovieAdapter);
+
+        rView.setLayoutManager(new LinearLayoutManager(this));
+
 
         AsyncHttpClient client = new AsyncHttpClient();
         client.get(sourceUrl, new JsonHttpResponseHandler() {
@@ -38,7 +52,8 @@ public class MainActivity extends AppCompatActivity {
                 try {
                     JSONArray results = jsonObject.getJSONArray("results");
                     Log.i(tag, "Results: " + results.toString());
-                    movie = movies.fromJsonArray(results);
+                    movie.addAll(movies.fromJsonArray(results));
+                    MovieAdapter.notifyDataSetChanged();
                     Log.i(tag, "Movies: " + movie.size());
                 } catch (JSONException e) {
                     Log.e(tag, "Hit json exception", e);
---
+++ src/main/java/com/example/flixsterapp/adapters/movieAdapter.java
@@ -0,0 +1,71 @@
+package com.example.flixsterapp.adapters;
+
+import android.content.Context;
+import android.text.Layout;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.flixsterapp.R;
+import com.example.flixsterapp.models.movies;
+
+import java.util.List;
+
+public class movieAdapter extends RecyclerView.Adapter<movieAdapter.viewHolder> {
+
+    Context context;
+    List<movies> movie;
+
+    public movieAdapter(Context context, List<movies> movie){
+        this.context = context;
+        this.movie = movie;
+    }
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("MovieAdapter", "onCreateViewHolder");
+        View movieView = LayoutInflater.from(context).inflate(R.layout.list_movie, parent, false);
+        return new viewHolder(movieView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+        Log.d("MovieAdapter", "onBindViewHolder " + position);
+        movies movies = movie.get(position);
+
+        holder.bind(movies);
+    }
+
+    @Override
+    public int getItemCount() {
+        return movie.size();
+    }
+
+    public class viewHolder extends RecyclerView.ViewHolder{
+
+        TextView movieTitle;
+        TextView movieOverview;
+        ImageView posterImage;
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieTitle = itemView.findViewById(R.id.tvTitle);
+            movieOverview = itemView.findViewById(R.id.tvOverview);
+            posterImage = itemView.findViewById(R.id.tvImage);
+        }
+
+        public void bind(movies movies) {
+            movieTitle.setText(movies.getTitle());
+            movieOverview.setText(movies.getOverview());
+            Glide.with(context).load(movies.getPosterPath()).into(posterImage);
+        }
+    }
+}
