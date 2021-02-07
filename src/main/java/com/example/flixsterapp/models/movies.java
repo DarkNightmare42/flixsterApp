8e957fbe28d9e92e8b40f4aa028b432a043e49ab
Errol Roberts <errol.me@gmail.com>
2021-02-06 20:19:23 -0500

    new code, no http response yet

--- src/main/java/com/example/flixsterapp/models/movies.java
+++ src/main/java/com/example/flixsterapp/models/movies.java
@@ -1,4 +1,42 @@
 package com.example.flixsterapp.models;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class movies {
+
+    String posterPath;
+    String title;
+    String overview;
+
+    public movies(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<movies> fromJsonArray(JSONArray movieJsonArray) throws JSONException {
+        List<movies> movie =  new ArrayList<>();
+        for(int i = 0; i < movieJsonArray.length(); i++){
+            movie.add(new movies(movieJsonArray.getJSONObject(i)));
+        }
+        return movie;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342/%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
 }
